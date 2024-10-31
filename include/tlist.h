322fef228fb5f3e1038f8bdfcd1382641f473f4d
vassyusha <romanoffmiss@yandex.ru>
2024-11-01 02:23:34 +0300

    class TList done

---
+++ include/tlist.h
@@ -0,0 +1,192 @@
+template<typename T>
+class TList {
+protected:
+	struct Node {
+		Node* next;
+		T value;
+
+		Node(Node* next, T value):next(next), value(value){}
+	};
+	size_t sz = 0;
+	Node* first = nullptr;
+
+public:
+
+	TList(int n = 0) {//constructor
+		if (n < 0) throw "incorrect size";
+		this->sz = n;
+		while (n--) {
+			Node* temp = new Node(this->first, 0);
+			this->first = temp;
+		}
+	}
+
+	~TList() {//destructor
+		Node* curr;
+		while (this->first != nullptr) {
+			curr = this->first->next;
+			delete this->first;
+			this->first = curr;
+		}
+		sz = 0;
+	}
+
+	TList(const TList& other) {//copy constructor
+		if (other.sz != 0) {
+			this->sz = other.sz;
+			Node* curr1 = other.first; //идем по элементам копируемого списка
+			Node* curr2 = new Node(nullptr, curr1->value); //то, куда копируем элементы списка
+			this->first = curr2;
+
+			while (!curr1) {//пока элемент существует
+				Node* next1 = curr1->next; 
+				if (!next1) break; //проверяем есть ли в копируемом списке следующий элемент
+				curr2->next = new Node(nullptr, next1->value); //если есть, то копируем следующий элемент
+				curr2 = curr2->next; //двигаемся по копируемому списку
+				curr1 = curr1->next; //и по тому, куда копируем
+			}
+		}
+	}
+
+	TList& operator=(const TList& other) {//operator=
+		if(this == &other) return *this;
+		this->sz = other.sz;
+		if (other.sz != 0) {
+			Node* curr1 = other.first;
+			Node* curr2 = new Node(nullptr, curr1->value);
+			this->first = curr2;
+
+			while (!curr1) {
+				Node* next1 = curr1->next; 
+				if (!next1) break; 
+				curr2->next = new Node(nullptr, next1->value); 
+				curr2 = curr2->next;
+				curr1 = curr1->next;
+			}
+		}
+		return *this;
+	}
+
+	TList(TList&& other) {//move constructor
+		this->sz = other.sz;
+		this->first = other.first;
+
+		other.sz = 0;
+		other.first = nullptr;
+	}
+
+	TList& operator=(TList&& other) {
+		this->sz = other.sz;
+		this->first = other.first;
+
+		other.sz = 0;
+		other.first = nullptr;
+
+		return *this;
+	}
+
+	size_t size() const {
+		return this->sz;
+	}
+
+	class iterator {
+	public:
+		Node* it = nullptr;
+		TList* list = nullptr;
+	public:
+		iterator(Node* node, TList* list): it(node), list(list){}
+
+		Node& operator*() { return *(this->it); }
+		Node operator*() const { return *(this->it); }
+
+		iterator& operator++() {
+			this->it = this->it->next;
+			return *this;
+		}
+
+		bool operator!=(iterator iter) const {
+			return (this->it != iter.it);
+		}
+
+		bool operator==(iterator iter) const {
+			return (this->it == iter.it);
+		}
+	};
+
+	iterator begin() {
+		return iterator(this->first, this);
+	}
+
+	iterator end() {
+		Node* curr = this->first;
+		if (curr) {
+			while (curr->next) {
+				curr = curr->next;
+			}
+		}
+		return iterator(curr, this);
+	}
+
+	T& at(iterator it) { return (*it).value; }
+	T at(iterator it) const { return (*it).value; }
+
+	//insert'ы и erase'ы
+
+	iterator insert_after(T data, iterator prev) {
+		Node* tmp = new Node(nullptr, data);
+		tmp->next = prev.it->next;
+		prev.it->next = tmp;
+		return iterator(tmp, this);
+	}
+
+	iterator insert_front(T data) {
+		this->first = new Node(first, data);
+		return iterator(this->first, this);
+	}
+
+	iterator erase_after(iterator prev) {
+		Node* tmp = prev.it->next;
+		if (tmp) {
+			prev.it->next = prev.it->next->next;
+			delete tmp;
+		}
+		return iterator(prev.it->next, this);
+	}
+
+	iterator erase_front() {
+		Node* tmp = this->first->next;
+		delete this->first;
+		this->first = tmp;
+		return iterator(this->first, this);
+	}
+
+	//сравнение
+	bool operator==(const TList& other) {
+		if (this->sz != other.sz) return false;
+
+		Node* curr1 = this->first, curr2 = other.first;
+		for (int i = 0; i < this->sz; i++) {
+			if (curr1 != curr2) return false;
+			curr1 = curr1->next;
+			curr2 = curr2->next;
+		}
+		return true;
+	}
+
+	bool operator!=(const TList& other) {
+		return !((*this) == other);
+	}
+
+	//friend ostream& operator<<(ostream& ostr, const TList& l)
+	//{
+	//	for (TList::iterator it = l.begin(); it != l.end(); ++it) {
+	//		ostr << (*it)->value << " "
+	//	}
+	//	//Node* curr = this->first;
+	//	//while (curr) {
+	//	//	ostr << curr->value << " ";
+	//	//	curr = curr->next;
+	//	//}
+	//	return ostr;
+	//}
+};
---
+++ test/test_tlist.cpp
@@ -0,0 +1,201 @@
+#include "tlist.h"
+
+#include <gtest.h>
+
+TEST(TList, can_create_list)
+{
+  ASSERT_NO_THROW(TList<int> l);
+}
+
+TEST(TList, can_create_list_with_positive_length)
+{
+	ASSERT_NO_THROW(TList<int> l(3));
+}
+
+TEST(TList, cant_create_list_with_negative_length)
+{
+	ASSERT_ANY_THROW(TList<int> l(-1));
+}
+
+TEST(TList, can_create_copied_list)
+{
+	TList<int> l1(3);
+	ASSERT_NO_THROW(TList<int> l(l1));
+}
+
+TEST(TList, can_get_size)
+{
+	TList<int> l1(3);
+	EXPECT_EQ(3, l1.size());
+}
+
+TEST(TList, can_set_and_get_node)
+{
+	TList<int> l1(3);
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	i = 0;
+	for (TList<int>::iterator it1 = l1.begin(); it1 != l1.end(); ++it1) {
+		EXPECT_EQ(l1.at(it1), i++);
+	}
+}
+
+TEST(TList, copied_list_is_equal_to_source_one)
+{
+	TList<int> l1(3);
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	TList<int> l2(l1);
+	EXPECT_EQ(l1.size(), l2.size());
+	for (TList<int>::iterator it1 = l1.begin(), it2 = l2.begin(); it1 != l1.end() && it2 != l2.end(); ++it1, ++it2) {
+		EXPECT_EQ(l1.at(it1), l2.at(it2));
+	}
+
+}
+
+TEST(TList, copied_list_has_its_own_memory)
+{
+	TList<int> l1(3);
+	TList<int> l2(l1);
+	EXPECT_NE(l1.begin(), l2.begin());
+}
+
+TEST(TList, can_assign_list_to_itself)
+{
+	TList<int> l1(3);
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	l1 = l1;
+	EXPECT_EQ(l1.size(), 3);
+	i = 0;
+	for (TList<int>::iterator it1 = l1.begin(); it1 != l1.end(); ++it1) {
+		EXPECT_EQ(l1.at(it1), i++);
+	}
+}
+
+TEST(TList, can_assign_lists)
+{
+	TList<int> l1(3), l2(4);
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	l2 = l1;
+	EXPECT_EQ(l2.size(), 3);
+	i = 0;
+	for (TList<int>::iterator it2 = l2.begin(); it2 != l2.end(); ++it2) {
+		EXPECT_EQ(l2.at(it2), i++);
+	}
+}
+
+TEST(TList, can_create_list_using_move_semantic)
+{
+	TList<int> l1(3);
+	ASSERT_NO_THROW(TList<int> l2(std::move(l1)));
+}
+
+TEST(TList, list_is_equal_to_copymoved_one)
+{
+	TList<int> l1(3);
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	TList<int> l2(std::move(l1));
+	EXPECT_EQ(3, l2.size());
+	i = 0;
+	for (TList<int>::iterator it2 = l2.begin(); it2 != l2.end(); ++it2) {
+		EXPECT_EQ(i++, l2.at(it2));
+	}
+
+}
+
+TEST(TList, copymoved_list_is_deleted)
+{
+	TList<int> l1(3);
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	TList<int> l2(std::move(l1));
+	EXPECT_EQ(0, l1.size());
+	EXPECT_EQ(l1.begin(), l1.end());
+
+}
+
+TEST(TList, can_move_list)
+{
+	TList<int> l1(3), l2;
+	ASSERT_NO_THROW(l2 = std::move(l1));
+}
+
+TEST(TList, list_is_equal_to_moved_one)
+{
+	TList<int> l1(3), l2;
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	l2 = std::move(l1);
+	EXPECT_EQ(3, l2.size());
+	i = 0;
+	for (TList<int>::iterator it2 = l2.begin(); it2 != l2.end(); ++it2) {
+		EXPECT_EQ(i++, l2.at(it2));
+	}
+
+}
+
+TEST(TList, moved_list_is_deleted)
+{
+	TList<int> l1(3), l2;
+	int i = 0;
+	for (TList<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		l1.at(it) = i++;
+	}
+	l2 = std::move(l1);
+	EXPECT_EQ(0, l1.size());
+	EXPECT_EQ(l1.begin(), l1.end());
+
+}
+
+TEST(TList, can_insert_front_element)
+{
+	TList<int> l;
+	l.insert_front(3);
+	EXPECT_EQ(l.at(l.begin()), 3);
+}
+
+TEST(TList, can_insert_element)
+{
+	TList<int> l;
+	l.insert_front(3);
+	TList<int>::iterator it = l.begin();
+	l.insert_after(2, it);
+	EXPECT_EQ(l.at(it), 3);
+}
+
+TEST(TList, can_erase_front_element)
+{
+	TList<int> l;
+	l.insert_front(3);
+	TList<int>::iterator it = l.begin();
+	l.insert_after(2, it);
+	l.erase_front();
+	EXPECT_EQ(l.at(l.begin()), 2);
+}
+
+TEST(TList, can_erase_element)
+{
+	TList<int> l;
+	l.insert_front(3);
+	TList<int>::iterator it = l.begin();
+	l.insert_after(2, it);
+	l.erase_after(l.begin());
+	EXPECT_EQ(l.at(l.begin()), 3);
+}
